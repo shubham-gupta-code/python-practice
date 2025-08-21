cb3d21c7237773f39205d89a5df5f593d33cba9e
shubham-gupta-code <shubham.it7062@gmail.com>
2025-08-21 15:22:33 +0530

    Add files via upload

---
+++ python-practice/problem3.py
@@ -0,0 +1,11 @@
+# Q3. Write a program to check if a number is even or odd.
+
+num = 45623
+
+if num%2==0:   # 'if' is a statement which checks the condition, if True then execute or if False then skips that block of code
+    print("Given number is an even number.")
+else:   # 'else' statement is used to execute a block of code, if any above code not executed 
+    print("Given number is an odd number.")
+
+# '%' is used to get remainder between division of two numbers e.g:- 12%2 is 0
+# even numbers are those who can be divisible by 2, remaining numbers are odd
