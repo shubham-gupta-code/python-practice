4d10886e31a352c52e0f1f4a6b1478905beda4d1
shubham-gupta-code <shubham.it7062@gmail.com>
2025-08-22 09:08:32 +0530

    Delete python-practice directory

--- python-practice/problem1.py
+++
@@ -1,9 +0,0 @@
-# Q1. Write a Python program to swap two variables.
-
-a = 10
-b = 20
-
-a, b = b, a   # a becomes b and b becomes a
-
-print("a =", a)   # print() is used to print anything
-print("b =", b)
--- python-practice/problem2.py
+++
@@ -1,7 +0,0 @@
-# Q2. Take user input and display it back to the user.
-
-user_input = input("Enter something: ")   # input() takes values given by user in a string type
-print(f"You type: {user_input}")
-
-# 'f' --> format strings
-# 'f' makes strings able to use variables of any data types.
